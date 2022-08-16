ca0c0b2cff504163016d298070bd118184f51642
gesht1 <gebriegashaw7@gmail.com>
2022-08-16 12:40:42 +0300

    hkh

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from "../styles/navbar.module.css"
 
 
 function Navbar() {
   return (
     <div>
+      <div className={styles.navbar}>
     <Link href='/about'> about</Link>
     <Link href='/profile'> profile</Link>
+    <Link href='/profile'> profile</Link>
+    </div>
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@ import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  <Layout>return <Component {...pageProps} /> </Layout>
+  return <Layout> 
+    <Component {...pageProps} />
+     </Layout>
 }
 
 export default MyApp
